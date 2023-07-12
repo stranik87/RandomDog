6cf9578dfd1fef55de1cc71f1e8fad55c8c2d9ff
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-12 17:57:30 +0500

    send msg

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+from telegram import Bot
+
+TOKEN = "5922381162:AAHZlv7P-uypWE8IzpQd0F6pZq9vex_IhyI"
 
 app = Flask(__name__)
+bot = Bot(token=TOKEN)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,6 +14,10 @@ def hello_world():
 
     elif request.method == 'POST':
         update = request.get_json()
-        print(update)
+        chat_id = update['message']['chat']['id']
+        text = 'Assalomu alaykum!'
+
+        bot.send_message(chat_id=chat_id, text=text)
+
         return 'Got a POST request!'
 
--- setting_webhook.py
+++ setting_webhook.py
@@ -1,17 +1,7 @@
 from telegram import Bot
 
-TOKEN = "6130157096:AAHgjNJMWeQhYdT5mMJd3jQgFJgz8O7JBtA"
+TOKEN = "5922381162:AAHZlv7P-uypWE8IzpQd0F6pZq9vex_IhyI"
 
 bot = Bot(token=TOKEN)
 
-
-def get_info():
-    print(bot.get_webhook_info())
-
-def seting_webhook():
-    print(bot.set_webhook("https://echobotdeploy.pythonanywhere.com/"))
-
-def delete_webhook():
-    print(bot.delete_webhook())
-
-get_info()
+print(bot.set_webhook("https://echobotdeploy.pythonanywhere.com"))
